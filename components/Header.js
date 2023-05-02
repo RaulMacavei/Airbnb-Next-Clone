ba5f082e754ef7159670c8a0b24875c358a11158
Raul Macavei <raulmacavei@yahoo.com>
2023-05-02 18:37:02 +0300

    Search functionality

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,32 @@
 import Image from "next/image";
+import { useState } from "react";
 import { BiSearch, BiGlobe, BiMenu, BiUser } from "react-icons/bi";
+import { HiUsers } from "react-icons/hi";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { DateRangePicker } from "react-date-range";
 
 function Header() {
+  const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [numOfGuests, setNumOfGuests] = useState(1);
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const resetInput = () => {
+    setSearchInput("");
+  };
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* Left */}
@@ -16,6 +41,8 @@ function Header() {
       {/* Middle */}
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
         <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
           type="text"
           placeholder="Start your search"
           className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
@@ -34,6 +61,41 @@ function Header() {
           <BiUser className="cursor-pointer text-[18px]" />
         </div>
       </div>
+
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
+          />
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of Guests
+            </h2>
+
+            <HiUsers className="h-5" />
+            <input
+              value={numOfGuests}
+              onChange={(e) => setNumOfGuests(e.target.value)}
+              min={1}
+              max={8}
+              type="number"
+              className="w-12 pl-2 text-lg outline-none text-red-400"
+            />
+          </div>
+          <div className="flex justify-around items-center">
+            <buttons
+              onClick={resetInput}
+              className="text-gray-500 cursor-pointer"
+            >
+              Cancel
+            </buttons>
+            <buttons className="text-red-400 cursor-pointer">Submit</buttons>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
--- components/SmallCards.js
+++ components/SmallCards.js
@@ -5,7 +5,7 @@ const SmallCard = ({ img, location }) => {
   return (
     <div className="flex items-center m-1 mt-4 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition duration-200 ease-out">
       <div className="relative h-16 w-16">
-        <Image src={img} fill className="rounded-lg" objectFit="cover" />
+        <Image src={img} fill className="rounded-lg object-cover" />
       </div>
       <div className="font-semibold">
         <h2>{location}</h2>
